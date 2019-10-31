3098e9e85e05c3fb5290a0cb76df1aab653cb507
Alex <a.kuturkin@mail.ru>
2019-10-31 10:51:40 +0300

    UP
    31.10 10:51

--- files/js/script.js
+++ files/js/script.js
@@ -40,3 +40,74 @@ const menuItem = new MenuItem;
 menuItem.makeActive(aboutFond,liFond);
 menuItem.makeActive(aboutFamily,liFamily);
 menuItem.makeActive(aboutHelp,liHelp);
+
+
+
+// код карты
+function init () {
+    var myMap = new ymaps.Map("map", {
+            center: [54.83, 37.11],
+            zoom: 5
+        }, {
+            searchControlProvider: 'yandex#search'
+        }),
+        myPlacemark = new ymaps.Placemark([56.907228, 31.260503], {
+            // Чтобы балун и хинт открывались на метке, необходимо задать ей определенные свойства.
+            balloonContentHeader: "Балун метки",
+            balloonContentBody: "Описание<br><img src='https://images.unsplash.com/photo-1495475163540-aea45ea95aed?w=150'>",
+            balloonContentFooter: "Подвал",
+            hintContent: "Хинт метки"
+        }, {
+            // Опции.
+            // Необходимо указать данный тип макета.
+            iconLayout: 'default#image',
+            // Своё изображение иконки метки.
+            iconImageHref: './images/map__mark_inactive.png',
+            // Размеры метки.
+            iconImageSize: [22, 24],
+            // Смещение левого верхнего угла иконки относительно
+            // её "ножки" (точки привязки).
+            iconImageOffset: [0, 0],
+            hideIconOnBalloonOpen: false
+        }),
+        myPlacemark2 = new ymaps.Placemark([56.907228, 36.260503], {
+            balloonContentBody: "<img class=f src='https://images.unsplash.com/photo-1495475163540-aea45ea95aed?w=150'>",
+            balloonContentFooter: "Подвал",
+            hintContent: "Хинт метки",
+        }, {
+            // Опции.
+            // Необходимо указать данный тип макета.
+            iconLayout: 'default#image',
+            // Своё изображение иконки метки.
+            iconImageHref: './images/map__mark_inactive.png',
+            // Размеры метки.
+            iconImageSize: [22, 24],
+            // Смещение левого верхнего угла иконки относительно
+            // её "ножки" (точки привязки).
+            iconImageOffset: [0, 0],
+            hideIconOnBalloonOpen: false
+        });;
+    
+    myMap.geoObjects.add(myPlacemark);
+    myMap.geoObjects.add(myPlacemark2);
+
+    myMap.events
+		.add(['balloonopen', 'balloonclose'], function (event) {
+	var target = event.get('target');
+	if (target.geometry && typeof target.getGeoObjects) {
+        if(event.get('type') == 'balloonopen') {
+            target.options.set({
+                iconImageHref: './images/map__mark_active.png'
+            });       
+    } else {
+	   target.options.set({
+           iconImageHref: './images/map__mark_inactive.png'
+     }); 
+    }
+}
+
+});
+}
+
+
+ymaps.ready(init);
